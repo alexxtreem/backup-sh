bd6a9557aa0cd84c8e7cec6cb6db35c888033371
alex <alexxtreem@gmail.com>
2022-02-13 15:57:13 +0300

    Init new repo

---
+++ backup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#Back script for files
+#
+#Set variables
+startTime=$(date +%Y-%m-%d_%H-%M-%S)
+cloudStorage=/mnt/s3selectl
+sourceDir=$(echo $1 | sed 's/\/\+$//')
+backUpdir=$(echo $2 | sed 's/\/\+$//')
+#
+cd $backUpdir
+echo "Backup is started at $startTime for $HOSTNAME"
+dirname=$startTime'_'$HOSTNAME
+tar -zcvPf $backUpdir/$dirname.tar.gz $sourceDir/
+openssl enc -aes-256-cbc -in $backUpdir/$dirname.tar.gz -out $backUpdir/$dirname.tar.gz.enc -aes-256-cbc -a -pass file:/root/myfile
+if [ -f $dirname.tar.gz.enc ]
+then
+	rm $dirname.tar.gz
+fi
+
+rsync -azvP $backUpdir/$dirname.tar.gz.enc $coudStorage/$dirname.tar.gz.enc
+
+if [ -f $cloudStorage/$dirname.tar.gz.enc ]
+then
+	rm $backUpdir/$dirname.tar.gz.enc
+fi
