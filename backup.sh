8fe8a8d3f74d61821b4b3fd57cf72bca3897f8d6
alexxtreem <77492405+alexxtreem@users.noreply.github.com>
2022-02-14 12:53:07 +0300

    Rsync feature (#1)
    * Rsync not fixed, add time when script finish it works
    
    * Fix path to cloud storage, do note resolve rsync trouble
    
    * Fixed rsync problem
    
    * Fix some bags, and add some descriptions
    
    * Fix some bags, add some description

--- backup.sh
+++ backup.sh
@@ -4,24 +4,40 @@
 #Set variables
 startTime=$(date +%Y-%m-%d_%H-%M-%S)
 #Path to S3 folder
-cloudStorage=/mnt/s3selectl
+cloudStorage=/mnt/s3selectel
 #Add backup dir, and dir where we are would place our backup files
 sourceDir=$(echo $1 | sed 's/\/\+$//')
 backUpdir=$(echo $2 | sed 's/\/\+$//')
 #
+echo "### * ### * ###"
 cd $backUpdir
 echo "Backup is started at $startTime for $HOSTNAME"
+echo "### * ### * ###"
 dirname=$startTime'_'$HOSTNAME
+echo "Start archiving suorce directiorie $backUpdir"
 tar -zcvPf $backUpdir/$dirname.tar.gz $sourceDir/
+
+echo "### * ### * ###"
+echo "Start encription source file"
 openssl enc -aes-256-cbc -in $backUpdir/$dirname.tar.gz -out $backUpdir/$dirname.tar.gz.enc -aes-256-cbc -a -pass file:/root/myfile
+echo "### * ### * ###"
+echo "If encription success, remove archive" 
 if [ -f $dirname.tar.gz.enc ]
 then
 	rm $dirname.tar.gz
 fi
 
-rsync -azvP $backUpdir/$dirname.tar.gz.enc $coudStorage/$dirname.tar.gz.enc
+echo "Check dirs $backUpdir $cloudStorage"
+echo "Backup files to S3 storage."
+rsync -avz --delete $backUpdir/$dirname.tar.gz.enc $cloudStorage
 
+echo "Check coping files to S3 storage and remove source."
 if [ -f $cloudStorage/$dirname.tar.gz.enc ]
 then
 	rm $backUpdir/$dirname.tar.gz.enc
+else
+	echo "File $backUpdir/$dirname.tar.gz.enc dose not exist"
 fi
+
+endTime=$(date +%Y-%m-%d_%H-%M-%S)
+echo "Backup is finished at $endTime for $HOSTNAME"
